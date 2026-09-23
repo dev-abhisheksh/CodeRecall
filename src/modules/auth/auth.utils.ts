be899b2df300c923c3fc37f484fe0feb905a5c16
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-09-23 23:48:57 +0530

    feat: add JWT utility functions for access and refresh token generation

---
+++ src/modules/auth/auth.utils.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const generateAccessToken = (userId: string): string => {
+    return jwt.sign(
+        { userId },
+        process.env.ACCESS_TOKEN_SECRET!,
+        { expiresIn: "15m" }
+    )
+}
+
+const generateRefreshToken = (userId: string): string => {
+    return jwt.sign(
+        { userId },
+        process.env.REFRESH_TOKEN_SECRET!,
+        { expiresIn: "7d" }
+    )
+}
+
+export {
+    generateAccessToken,
+    generateRefreshToken
+}
