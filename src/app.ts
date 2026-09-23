6139ac443619b28164e0a8fc92a717d2b5586428
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-09-24 00:08:26 +0530

    feat: add authentication routes for user registration

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/error.middleware.js";
+import authRoutes from "./modules/auth/auth.routes.js"
 
 dotenv.config();
 
@@ -9,6 +10,10 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+
+// Routes
+
+app.use("/api/auth", authRoutes)
 
 
 app.use(errorHandler)
