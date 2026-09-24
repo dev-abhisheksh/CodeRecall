8f76117dde41a089049bd5bae1bbb43906e19741
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-09-24 21:05:39 +0530

    feat: implement user login functionality with JWT token generation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import User from "../user/user.model.js";
 import ApiError from "../../errors/ApiError.js";
 import bcrypt from "bcryptjs";
 import { generateAccessToken, generateRefreshToken } from "./auth.utils.js";
+import LoginUser from "../../types/auth/loginUser.js";
 
 const registerUser = asyncHandler(async (req: Request, res: Response): Promise<void> => {
     const { username, fullName, email, password } = req.body as RegisterUser;
@@ -19,9 +20,6 @@ const registerUser = asyncHandler(async (req: Request, res: Response): Promise<v
     const user = await User.findOne({ email: sanitizedData.email });
     if (user) throw new ApiError(400, "user alredy exists")
 
-    const hashPass = await bcrypt.hash(password, 10);
-
-    sanitizedData.password = hashPass;
 
     const newUser = await User.create(sanitizedData)
 
@@ -49,6 +47,44 @@ const registerUser = asyncHandler(async (req: Request, res: Response): Promise<v
         })
 })
 
+const loginUser = asyncHandler(async (req: Request, res: Response): Promise<void> => {
+    const { email, password } = req.body as LoginUser;
+
+    if (!email || !password) throw new ApiError(400, "Both fields are required")
+
+    const user = await User.findOne({ email })
+        .select("+password")
+
+    if (!user) throw new ApiError(400, "User not found. Please register")
+
+    const isPassCorrect = await user.comparePassword(password);
+    if (!isPassCorrect) throw new ApiError(400, "Invalid email or password")
+
+    const accessToken = generateAccessToken(user._id.toString())
+    const refreshToken = generateRefreshToken(user._id.toString());
+
+    res
+        .cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 15 * 60 * 1000
+        })
+
+        .cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        .status(200)
+        .json({
+            success: true,
+            message: "user logged in successfully"
+        })
+})
+
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
